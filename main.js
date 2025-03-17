4e757f21335d7b270578ee4588e3c1629cd9d7f1
fluffy <121191983+fluffyydev@users.noreply.github.com>
2025-03-17 19:17:53 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,21 +1,16 @@
 const canvas = document.getElementById('theCanvas');
-const ctx = canvas.getContext('2d');
-
 canvas.fillStyle = 'white';
 
-context.font = '20px Arial';
-context.fillStyle = 'green';
-context.fillText('helo', 50, 300);
-
 // Function taken from W3Schools .. I added scalability
-function draw(length) {
+function draw(length, pixelSize) {
+    const ctx = document.getElementById('theCanvas').getContext("2d");
     for (let i = 0; i < length; i++) {
-        for (let j = 0; j < length; j++) {
-            ctx.fillStyle = `rgb(${Math.floor(255 - (255 / length) * i)} ${Math.floor( 255 - (255 / length) * j,)} 255)`;
-            ctx.fillRect(j * 25, i * 25, 25, 25);
-        }
+      for (let j = 0; j < length; j++) {
+        ctx.fillStyle = `rgb(${Math.floor(255 - (255 / length) * i)} ${Math.floor( 255 - (255 / length) * j,)} 255)`;
+        ctx.fillRect(j * pixelSize, i * pixelSize, pixelSize, pixelSize);
+      }
     }
-}
+  }
 
 // Adding a scale bar later
-draw(50)
+draw(10, 10)
