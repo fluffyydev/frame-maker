2c289d4f0b760facd60d71dc8ddd7eca5db0fde4
fluffy <121191983+fluffyydev@users.noreply.github.com>
2025-03-16 23:26:01 -0400

    Update and rename script.js to main.js

--- main.js
+++ main.js
@@ -10,10 +10,11 @@ context.fillText('helo', 50, 300);
 function draw(length) {
     const ctx = document.getElementById("gameCanvas").getContext("2d");
     for (let i = 0; i < length; i++) {
-      for (let j = 0; j < length; j++) {
-        ctx.fillStyle = `rgb(${Math.floor(255 - (255 / length) * i)} ${Math.floor( 255 - (255 / length) * j,)} 255)`;
-        ctx.fillRect(j * 25, i * 25, 25, 25);
-      }
+        for (let j = 0; j < length; j++) {
+            ctx.fillStyle = `rgb(${Math.floor(255 - (255 / length) * i)} ${Math.floor( 255 - (255 / length) * j,)} 255)`;
+            ctx.fillRect(j * 25, i * 25, 25, 25);
+        }
     }
-  }
+}
+
 draw(50)
