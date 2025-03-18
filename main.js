cbc254025d3a3c0884de1a2616110c613ad86ce5
fluffy <121191983+fluffyydev@users.noreply.github.com>
2025-03-17 22:31:09 -0400

    Added updateScreen function

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const canvas = document.getElementById('theCanvas');
 canvas.fillStyle = 'white';
 
-// Function taken from W3Schools .. I added scalability
+// Function taken from W3Schools - I added scalability
 function draw(length, pixelSize) {
     const ctx = document.getElementById('theCanvas').getContext("2d");
     for (let i = 0; i < length; i++) {
@@ -14,3 +14,11 @@ function draw(length, pixelSize) {
 
 // Adding a scale bar later
 draw(10, 10)
+
+function updateScreen() {
+    const canvas = document.getElementById('theCanvas');
+    const ctx = canvas.getContext("2d");
+    // Clearing canvas 
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    // e
+}
