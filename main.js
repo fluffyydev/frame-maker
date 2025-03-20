f68d39f18dd2004e30f886b5a9b02d59c022f00f
fluffy <121191983+fluffyydev@users.noreply.github.com>
2025-03-19 22:40:01 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,48 +1,63 @@
 const canvas = document.getElementById('theCanvas');
 canvas.fillStyle = 'white';
 
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
 // Sliders and Buttons
-var redHueSelector = document.getElementById("RDirection")
-var greenHueSelector = document.getElementById("gDirection")
-var blueHueSelector = document.getElementById("RDirection")
+var redHueSelector = document.getElementById("RDirection");
+var greenHueSelector = document.getElementById("GDirection");
+var blueHueSelector = document.getElementById("BDirection");
 
-var rSlider = document.getElementById("rRange")
-var gSlider = document.getElementById("gRange")
-var bSlider = document.getElementById("bRange")
+var rSlider = document.getElementById("rRange");
+var gSlider = document.getElementById("gRange");
+var bSlider = document.getElementById("bRange");
 
-var pixelSizeSlider = document.getElementById("pixelSizeRange")
-var pixelAmountSlider = document.getElementById("pixelAmountRange")
+var pixelSizeSlider = document.getElementById("pixelSizeRange");
+var pixelAmountSlider = document.getElementById("pixelAmountRange");
+var pixelAmountSlider2 = document.getElementById("pixelAmountRange2");
 
+var randomFrameButton = document.getElementById("randomButton");
 
 // Function taken from W3Schools - I added scalability
 function draw(length, pixelSize) {
+    var length2 = pixelAmountSlider2.value;
     const ctx = document.getElementById('theCanvas').getContext("2d");
     for (let i = 0; i < length; i++) {
-      for (let j = 0; j < length; j++) {
-        var rNum = 255;
+      for (let j = 0; j < length2; j++) {
+        var rNum = 0;
         var gNum = 255;
         var bNum = 255;
         if (redHueSelector.value == "left") {
-          rNum = Math.floor(255 - (255 / length) * j)
+          rSlider.hidden = true;
+          rNum = Math.floor(255 - (255 / length) * j);
         } else if (redHueSelector.value == "down") {
-          rNum = Math.floor(255 - (255 / length) * i)
+          rSlider.hidden = true;
+          rNum = Math.floor(255 - (255 / length) * i);
         } else {
+          rSlider.hidden = false;
           rNum = rSlider.value;
         }
 
         if (greenHueSelector.value == "left") {
-          gNum = Math.floor(255 - (255 / length) * j)
+          gSlider.hidden = true;
+          gNum = Math.floor(255 - (255 / length) * j);
         } else if (greenHueSelector.value == "down") {
-          gNum = Math.floor(255 - (255 / length) * i)
+          gSlider.hidden = true;
+          gNum = Math.floor(255 - (255 / length) * i);
         } else {
+          gSlider.hidden = false;
           gNum = gSlider.value;
         }
           
         if (blueHueSelector.value == "left") {
-          bNum = Math.floor(255 - (255 / length) * j)
+          bSlider.hidden = true;
+          bNum = Math.floor(255 - (255 / length) * j);
         } else if (blueHueSelector.value == "down") {
-          bNum = Math.floor(255 - (255 / length) * i)
+          bSlider.hidden = true;
+          bNum = Math.floor(255 - (255 / length) * i);
         } else {
+          bSlider.hidden = false;
           bNum = bSlider.value;
         }
         
@@ -50,6 +65,7 @@ function draw(length, pixelSize) {
         var rbgColor = "rgb(" + rNum + ", " + gNum + ", " + bNum + ")";
         
         ctx.fillStyle = rbgColor;
+        
         ctx.fillRect(j * pixelSize, i * pixelSize, pixelSize, pixelSize);
       }
     }
@@ -61,69 +77,52 @@ function updateScreen() {
   const ctx = canvas.getContext("2d");
   // Clearing canvas 
   ctx.clearRect(0, 0, canvas.width, canvas.height);
+  canvas.width = pixelAmountSlider2.value * pixelSizeSlider.value;
+  canvas.height = pixelAmountSlider.value * pixelSizeSlider.value;
 
-  draw(100, 1);
+  draw(pixelAmountSlider.value, pixelSizeSlider.value);
 }
 
-// Adding a scale bar later
 draw(pixelAmountSlider.value, pixelSizeSlider.value);
 
 
 rSlider.oninput = function() {
-  const canvas = document.getElementById('theCanvas');
-  const ctx = canvas.getContext("2d");
-  // Clearing canvas 
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-  draw(pixelAmountSlider.value, pixelSizeSlider.value);
+  updateScreen()
 }
 
 gSlider.oninput = function() {
-  const canvas = document.getElementById('theCanvas');
-  const ctx = canvas.getContext("2d");
-  // Clearing canvas 
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  updateScreen()
+}
 
-  draw(pixelAmountSlider.value, pixelSizeSlider.value);
+bSlider.oninput = function() {
+  updateScreen()
 }
 
 pixelSizeSlider.oninput = function() {
-  const canvas = document.getElementById('theCanvas');
-  const ctx = canvas.getContext("2d");
-  // Clearing canvas 
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-  draw(pixelAmountSlider.value, pixelSizeSlider.value);
+  updateScreen()
 }
 
 pixelAmountSlider.oninput = function() {
-  const canvas = document.getElementById('theCanvas');
-  const ctx = canvas.getContext("2d");
-  // Clearing canvas 
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  updateScreen()
+}
 
-  draw(pixelAmountSlider.value, pixelSizeSlider.value);
+pixelAmountSlider2.oninput = function() {
+  updateScreen()
 }
 
 redHueSelector.oninput = function() {
-  const canvas = document.getElementById('theCanvas');
-  const ctx = canvas.getContext("2d");
-  // Clearing canvas 
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-  draw(pixelAmountSlider.value, pixelSizeSlider.value);
+  updateScreen()
 }
 
-greeneHueSelector.oninput = function() {
-  const canvas = document.getElementById('theCanvas');
-  const ctx = canvas.getContext("2d");
-  // Clearing canvas 
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-  draw(pixelAmountSlider.value, pixelSizeSlider.value);
+greenHueSelector.oninput = function() {
+  updateScreen()
 }
 
 blueHueSelector.oninput = function() {
+  updateScreen()
+}
+
+randomFrameButton.oninput = function() {
   const canvas = document.getElementById('theCanvas');
   const ctx = canvas.getContext("2d");
   // Clearing canvas 
